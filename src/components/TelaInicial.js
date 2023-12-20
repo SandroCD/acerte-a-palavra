005755810a4b5d95eb7a3bc703af3188394b853b
SandroCD <sandrocd@gmail.com>
2023-12-20 12:09:13 -0300

    Componente Tela Inicial

---
+++ src/components/TelaInicial.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = ({inicio}) => {
+  return (
+    <div className="start">
+      <h1>Palavra Secreta</h1>
+      <p>Clique no botão abaixo para começar a jogar</p>
+      <button onClick={inicio}>Iniciar o jogo</button>
+    </div>
+  );
+};
+
+export default StartScreen;
